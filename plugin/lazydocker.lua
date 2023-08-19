5191f145346c57a10548c90ed10b522bd6e3ba6e
crnvl96 <adran@hey.com>
2023-08-19 16:21:39 -0300

    refact: changes entrypoint filetyle from .vim to .lua

---
+++ plugin/lazydocker.lua
@@ -0,0 +1,8 @@
+vim.api.nvim_create_user_command("LazyDocker", function()
+	if vim.fn.has("nvim-0.9") == 0 then
+		print("lazydocker.nvim needs Neovim >= 0.9")
+		return
+	end
+
+	require("lazydocker").toggle()
+end, {})
