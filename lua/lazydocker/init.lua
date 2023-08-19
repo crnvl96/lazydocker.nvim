7cb0633de11cbdc574c5755611168c93fabb4084
crnvl96 <adran@hey.com>
2023-08-19 00:07:47 -0300

    feat: calls recent added files in lazydocker.init

--- lua/lazydocker/init.lua
+++ lua/lazydocker/init.lua
@@ -1,7 +1,11 @@
+local util = require("lazydocker.util")
+local view = require("lazydocker.view")
 local M = {}
 
 function M.toggle()
 	print("Toggle lazydocker")
+	view.open()
+	util.exec()
 end
 
 return M
