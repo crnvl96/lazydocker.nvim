8acbafdd9ac494e8872bec6d3b01e8158b2b2af0
crnvl96 <adran@hey.com>
2023-08-19 00:17:54 -0300

    feat: incorporates config into init file

--- lua/lazydocker/init.lua
+++ lua/lazydocker/init.lua
@@ -1,11 +1,13 @@
 local util = require("lazydocker.util")
 local view = require("lazydocker.view")
+local config = require("lazydocker.config")
 local M = {}
 
 function M.toggle()
-	print("Toggle lazydocker")
+	print("Init lazydocker")
 	view.open()
 	util.exec()
+	config.config()
 end
 
 return M
