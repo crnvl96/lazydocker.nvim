e54550881942d171eabc677e3487a7df946d3222
Ádran Carnavale <adran.carnavale@gmail.com>
2023-08-19 19:45:23 -0300

    [FEAT]: mounts popup with initial message

--- lua/lazydocker/view.lua
+++ lua/lazydocker/view.lua
@@ -11,6 +11,8 @@ end
 function View:open()
 	self.docker_panel = Popup(config.options.popup_window)
 	self.docker_panel:mount()
+
+	vim.api.nvim_buf_set_lines(self.docker_panel.bufnr, 0, 1, false, { "LazyDocker will be rendered here" })
 end
 
 return View
