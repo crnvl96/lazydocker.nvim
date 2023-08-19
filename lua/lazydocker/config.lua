0e31c3649b32f5b908c61b9fa801d34de40a252d
Ádran Carnavale <adran.carnavale@gmail.com>
2023-08-19 17:49:31 -0300

    [FEAT]: Creates file with initial default configs

--- lua/lazydocker/config.lua
+++ lua/lazydocker/config.lua
@@ -1,7 +1,37 @@
 local M = {}
 
-M.load = function()
-	print("Load config")
+function M.defaults()
+	local defaults = {
+		-- TODO: Add config options here
+		popup_window = {
+			border = {
+				highlight = "FloatBorder",
+				style = "rounded",
+				text = {
+					top = " ChatGPT ",
+				},
+			},
+			win_options = {
+				wrap = true,
+				linebreak = true,
+				foldcolumn = "1",
+				winhighlight = "Normal:Normal,FloatBorder:FloatBorder",
+			},
+			buf_options = {
+				filetype = "markdown",
+			},
+		},
+	}
+	return defaults
+end
+
+M.options = {}
+
+M.namespace_id = vim.api.nvim_create_namespace("LazyDocker")
+
+function M.setup(options)
+	options = options or {}
+	M.options = vim.tbl_deep_extend("force", {}, M.defaults(), options)
 end
 
 return M
