8c47d9aed90027135f8cb357445949abaf1433aa
crnvl96 <adran@hey.com>
2023-08-20 14:14:13 -0300

    feat: removes nui check from utils.lua

--- lua/lazydocker/utils.lua
+++ lua/lazydocker/utils.lua
@@ -8,9 +8,4 @@ M.is_docker_available = function()
 	return vim.fn.executable("docker") == 1
 end
 
-M.is_nui_available = function()
-	local has_nui, _ = pcall(require, "nui")
-	return has_nui
-end
-
 return M
