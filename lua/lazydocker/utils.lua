c670d34426a74f3606b43058fa6872816aeea08e
Ádran Carnavale <adran.carnavale@gmail.com>
2023-08-20 13:56:44 -0300

    feat: adds helper function to check if nui is installed

--- lua/lazydocker/utils.lua
+++ lua/lazydocker/utils.lua
@@ -8,4 +8,9 @@ M.is_docker_available = function()
 	return vim.fn.executable("docker --version") == 1
 end
 
+M.is_nui_available = function()
+	local has_nui, _ = pcall(require, "nui")
+	return has_nui
+end
+
 return M
