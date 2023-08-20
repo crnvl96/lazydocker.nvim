a769d9fdafccd4004bff22b118765f5e10437dc7
Ádran Carnavale <adran.carnavale@gmail.com>
2023-08-20 14:35:35 -0300

    refact: disables user options for a while

--- lua/lazydocker/config.lua
+++ lua/lazydocker/config.lua
@@ -36,7 +36,9 @@ M.options = {}
 M.namespace_id = vim.api.nvim_create_namespace("LazyDocker")
 
 function M.setup(options)
-	options = options or {}
+	-- TODO: Change this to the commented line below, when the plugin accepts options
+	-- options = options or {}
+	options = {}
 	M.options = vim.tbl_deep_extend("force", {}, M.defaults(), options)
 end
 
