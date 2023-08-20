d3cfbaec975d374cf98ac2a6554d5b4f7324112a
crnvl96 <adran@hey.com>
2023-08-19 23:23:16 -0300

    [REFACT]: implements init function

--- lua/lazydocker/view.lua
+++ lua/lazydocker/view.lua
@@ -5,25 +5,8 @@ local config = require("lazydocker.config")
 
 local View = class({})
 
-function View:toggle()
-	self.docker_panel = Popup(config.options.popup_window)
-	self.docker_panel:mount()
-
-	vim.api.nvim_buf_set_lines(self.docker_panel.bufnr, 0, 1, false, { "LazyDocker will be rendered here" })
-
-	self.docker_panel:on(event.BufLeave, function()
-		self.docker_panel:unmount()
-	end)
-
-	self.docker_panel:map("n", "<esc>", function()
-		self.docker_panel:off("BufLeave")
-		self.docker_panel:unmount()
-	end, { noremap = true })
-
-	self.docker_panel:map("n", "q", function()
-		self.docker_panel:off("BufLeave")
-		self.docker_panel:unmount()
-	end, { noremap = true })
+function View:init()
+	self.popup = Popup(config.options.popup_window)
 end
 
 -- function View:toggle()
