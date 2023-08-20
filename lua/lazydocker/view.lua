9ffe6881bd67a246f09c7196f5db8e910377147e
Ádran Carnavale <adran.carnavale@gmail.com>
2023-08-19 23:00:44 -0300

    [REFACT]: tries strategy of setting autocmd off

--- lua/lazydocker/view.lua
+++ lua/lazydocker/view.lua
@@ -5,41 +5,27 @@ local config = require("lazydocker.config")
 
 local View = class({})
 
-function View:init()
-	self.active = false
-end
-
 function View:toggle()
-	if self.active == false then
-		self.docker_panel = Popup(config.options.popup_window)
-		self.docker_panel:mount()
+	self.docker_panel = Popup(config.options.popup_window)
+	self.docker_panel:mount()
 
-		vim.api.nvim_buf_set_lines(self.docker_panel.bufnr, 0, 1, false, { "LazyDocker will be rendered here" })
-		self.active = true
-	end
+	vim.api.nvim_buf_set_lines(self.docker_panel.bufnr, 0, 1, false, { "LazyDocker will be rendered here" })
 
-	-- self.docker_panel:on(event.BufLeave, function()
-	-- 	if self.active == true then
-	-- 		print("Close because of BufLeave")
-	-- 		self.docker_panel:unmount()
-	-- 		self.active = false
-	-- 	end
-	-- end)
+	self.docker_panel:on(event.BufLeave, function()
+		print("Close because of BufLeave")
+		self.docker_panel:unmount()
+	end, { once = true })
 
 	self.docker_panel:map("n", "<esc>", function()
-		if self.active == true then
-			print("Close because of <ESC>")
-			self.docker_panel:unmount()
-			self.active = false
-		end
+		print("Close because of <ESC>")
+		self.docker_panel:off("*")
+		self.docker_panel:unmount()
 	end, { noremap = true })
 
 	self.docker_panel:map("n", "q", function()
-		if self.active == true then
-			print("Close because of Q")
-			self.docker_panel:unmount()
-			self.active = false
-		end
+		print("Close because of Q")
+		self.docker_panel:off("*")
+		self.docker_panel:unmount()
 	end, { noremap = true })
 end
 
