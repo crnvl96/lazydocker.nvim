050af9d816fd5f034d46211bc7d617d5ecbaf89f
crnvl96 <adran@hey.com>
2023-08-20 18:52:50 -0300

    fix: default plugin config

--- lua/lazydocker/config.lua
+++ lua/lazydocker/config.lua
@@ -2,18 +2,23 @@ local M = {}
 
 function M.defaults()
 	local defaults = {
-		-- TODO: Add config options here
 		popup_window = {
 			enter = true,
 			focusable = true,
 			zindex = 40,
+			position = "50%",
 			relative = "win",
+			size = {
+				width = "90%",
+				height = "90%",
+			},
 			buf_options = {
 				modifiable = true,
 				readonly = false,
 			},
 			win_options = {
 				winhighlight = "Normal:Normal,FloatBorder:FloatBorder",
+				winblend = 0,
 			},
 			border = {
 				highlight = "FloatBorder",
@@ -21,11 +26,6 @@ function M.defaults()
 				text = {
 					top = " Lazydocker ",
 				},
-			},
-			position = "50%",
-			size = {
-				width = "90%",
-				height = "90%",
 			},
 		},
 	}
@@ -37,9 +37,7 @@ M.options = {}
 M.namespace_id = vim.api.nvim_create_namespace("LazyDocker")
 
 function M.setup(options)
-	-- TODO: Change this to the commented line below, when the plugin accepts options
-	-- options = options or {}
-	options = {}
+	options = options or {}
 	M.options = vim.tbl_deep_extend("force", {}, M.defaults(), options)
 end
 
