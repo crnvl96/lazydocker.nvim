cfde91fa1ff25f3589cf7a576646cf96dfb3ce0f
Ádran Carnavale <adran.carnavale@gmail.com>
2023-08-19 23:18:33 -0300

    [REFACT]: removes logs

--- lua/lazydocker/view.lua
+++ lua/lazydocker/view.lua
@@ -12,18 +12,15 @@ function View:toggle()
 	vim.api.nvim_buf_set_lines(self.docker_panel.bufnr, 0, 1, false, { "LazyDocker will be rendered here" })
 
 	self.docker_panel:on(event.BufLeave, function()
-		print("Close because of BufLeave")
 		self.docker_panel:unmount()
 	end)
 
 	self.docker_panel:map("n", "<esc>", function()
-		print("Close because of <ESC>")
 		self.docker_panel:off("BufLeave")
 		self.docker_panel:unmount()
 	end, { noremap = true })
 
 	self.docker_panel:map("n", "q", function()
-		print("Close because of Q")
 		self.docker_panel:off("BufLeave")
 		self.docker_panel:unmount()
 	end, { noremap = true })
