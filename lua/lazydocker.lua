b544568cad31a27f21d300d6872a8f602766ffec
Ádran Farias Carnavale <adran.carnavale@gmail.com>
2024-10-06 21:57:07 -0300

    refactor: initial setup for rewrite

--- lua/lazydocker.lua
+++ lua/lazydocker.lua
@@ -1,18 +1,52 @@
-local config = require("lazydocker.config")
-local module = require("lazydocker.module")
+-- Definitions.
+--
+-- Lazydocker module definition.
+local Lazydocker = {}
 
-local M = {}
+-- Config module definition
+local Config = {}
 
-M.setup = function(options)
-	config.setup(options)
+-- Generate default config
+Config.default_config = {
+	width = 0.5,
+	height = 0.5,
+}
+
+-- Setup function definition
+---@param config table|nil Module config table. See |Lazydocker.config|.
+---@usage >lua
+---   require('lazydocker').setup() -- Use default config.
+---   -- OR
+---   require('lazydocker').config({}) -- Provide your own config as a table.
+--- <
+function Lazydocker.setup(config)
+	_G.Lazydocker = Lazydocker
+
+	vim.validate({
+		config = {
+			config, -- arg value.
+			"table", -- type value.
+			true, -- true means that the arg is optional (can be nil).
+		},
+	})
+
+	-- Create a new instance of our default config table before extending it.
+	config = vim.tbl_deep_extend("force", vim.deepcopy(Config.default_config), config or {})
+
+	Lazydocker.config = config
 end
 
---
--- public methods for the plugin
---
+---@alias __lazydocker_return boolean Whether the operation has completed successfully.
 
-M.toggle = function()
-	module.toggle()
+-- Toggle Docker floating window
+---@return __lazydocker_return
+function Lazydocker.toggle()
+	print(Lazydocker.config.width)
+	print(Lazydocker.config.height)
+
+	print("Toggle!!!")
+
+	return true
 end
 
-return M
+return Lazydocker
